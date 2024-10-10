b510d0b6f01179a66809b0a2d3f035fdba4f6794
ToptalDev <proLove0212@gmail.com>
2024-10-10 10:57:23 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; 
 import App from './App';
 
 import './style.css';
